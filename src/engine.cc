e9b8fd33ac13051260e807535a726902cf44de77
MasterXLX <47871798+MasterXLX@users.noreply.github.com>
2026-08-20 18:06:07 -0400

    Refactor weight computation in engine.cc

--- src/engine.cc
+++ src/engine.cc
@@ -382,15 +382,11 @@ void ValueOnlyGo(NodeTree* tree, Network* network, const OptionsDict& options,
   int polidx = 0;
   float max_q = std::numeric_limits<float>::lowest();
 
-  inline float ComputeWeight(const SearchParams& params, float uncertainty) {
-  if (!params.GetUseUncertaintyWeighting()) return 1.0f;
+  const SearchParams params(options);
   const float cap = params.GetUncertaintyWeightingCap();
   const float coefficient = params.GetUncertaintyWeightingCoefficient();
   const float exponent = params.GetUncertaintyWeightingExponent();
   return fmin(cap, coefficient * pow(uncertainty, exponent));
-}
-
-  const SearchParams params(options);
 
   for (auto edge : tree->GetCurrentHead()->Edges()) {
     history.Append(edge.GetMove());
@@ -403,7 +399,7 @@ void ValueOnlyGo(NodeTree* tree, Network* network, const OptionsDict& options,
       // NN eval is from the side-to-move perspective, so if the child
       // position is good for the opponent, it is bad for us.
       q = -comp_q[comp_idx];
-      q /= ComputeWeight(params, comp_uncertainty[comp_idx]);
+      q /= fmin(cap, coefficient * pow(uncertainty, exponent));
       ++comp_idx;
     } else if (result == GameResult::DRAW) {
       q = 0.0f;
