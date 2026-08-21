abc0e1b7502d5ac8674cf242e9742268d908fd4d
MasterXLX <47871798+MasterXLX@users.noreply.github.com>
2026-08-21 12:39:35 -0400

    Refactor evaluation calculation in engine.cc

--- src/engine.cc
+++ src/engine.cc
@@ -398,7 +398,7 @@ void ValueOnlyGo(NodeTree* tree, Network* network, const OptionsDict& options,
       // NN eval is from the side-to-move perspective, so if the child
       // position is good for the opponent, it is bad for us.
       q = -comp_q[comp_idx];
-      q /= std::min(cap, coefficient * pow(comp_uncertainty[comp_idx], exponent));
+      q /= coefficient * comp_uncertainty[comp_idx] + 1 - coefficient / 2;
       ++comp_idx;
     } else if (result == GameResult::DRAW) {
       q = 0.0f;
